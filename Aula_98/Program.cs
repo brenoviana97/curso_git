0e0b6f71420e70ad2e05a8c678e4f53ddf7150cb
Breno Viana <brenoviana97@outlook.com>
2021-05-05 21:52:39 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ Aula_98/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula_98
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
